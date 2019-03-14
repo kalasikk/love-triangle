76fc32ebc4fd219e08989ec91d47f205503f7ac0
kalasikk <kalasikk@gmail.com>
2019-03-14 08:35:16 +0300

    docs: create perfect function

--- src/index.js
+++ src/index.js
@@ -4,4 +4,30 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var k=0;
+  var d=0;
+  for (let i = 0; i < preferences.length; i++) {
+    if(preferences[i]==preferences[0]){
+      d++;
+    }
+  }
+  if(d==preferences.length){
+    return 0;
+  }
+  preferences.splice(0,0,',');
+  for (let i = 1; i < preferences.length; i++) {
+    var tmp1,tmp2;
+    tmp1=preferences[i];
+    tmp2=preferences[tmp1];
+    if (preferences[tmp2]==i) {
+      if(preferences[tmp2]!=preferences[i]){
+      k++;
+      delete preferences[i];
+      delete preferences[tmp1];
+      delete preferences[tmp2];
+        }
+    }
+    
+  }
+  return k;
 };
